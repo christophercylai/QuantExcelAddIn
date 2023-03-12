a931b3fa25f49d2c7a21ad3c3d97d3760ef02c9e
clai <christophercylai@outlook.com>
2023-03-11 19:40:51 -0800

    cs calls python and display a UDF in excel OK

---
+++ main.cs
@@ -0,0 +1,16 @@
+﻿using ExcelDna.Integration;
+
+
+namespace qxlpy
+{
+    public static class ExcelFunc
+    {
+        [ExcelFunction(Name = "ReturnPath")]
+        public static string ReturnPath()
+        {
+            PyExecutor pye = new();
+            string ret = pye.GetPath();
+            return ret;
+        }
+    }
+}
