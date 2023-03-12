e3c02ff68b3dd8ee100e3237126b156a303cc1c3
Christopher Lai <33213350+christophercylai@users.noreply.github.com>
2023-03-11 23:30:59 -0500

    Issue 1 cs excel py (#2)
    * cs calls python and display a UDF in excel OK
    
    * update README

---
+++ python.cs
@@ -0,0 +1,33 @@
+﻿using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace qxlpy
+{
+    public class PyExecutor
+    {
+        private readonly PyModule scope;
+        public PyExecutor()
+        {
+            string python_dll = @"C:\Users\christophercylai\Python37\python37.dll";
+            Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", python_dll);
+            PythonEngine.Initialize();
+            scope = Py.CreateScope();
+        }
+
+        public string GetPath()
+        {
+            using (Py.GIL())
+            {
+                scope.Exec("print('HelloWorld!')");
+                dynamic os = scope.Import("os");
+                string path_env = os.getenv("PATH");
+                return path_env;
+            }
+        }
+    }
+}
