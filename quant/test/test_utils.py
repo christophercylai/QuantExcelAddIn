d7f8d9969abe57fe790be9148e64e7d5bd1aadac
Christopher Lai <33213350+christophercylai@users.noreply.github.com>
2023-04-21 00:05:24 -0400

    added plotting example (#24)
    * added plotting example
    
    * separate dframe creation and plot generation
    
    * setup.ps1: build and run qxlpy
    
    * setup.ps1 deven qxlpy.sln - dframe rm period

---
+++ quant/test/test_utils.py
@@ -0,0 +1,15 @@
+# pylint: disable=missing-module-docstring
+from .. import utils
+from .. import objects
+
+def test_plot_data():
+    # pylint: disable=missing-function-docstring
+    tdbank = objects.qxlpyStoreDoubleList([1, 2, 3])
+    bmo = objects.qxlpyStoreDoubleList([3, 2, 1])
+    boa = objects.qxlpyStoreDoubleList([1, 3, 2])
+    dframe = utils.qxlpyCreatePlotDataFrame(
+        [tdbank, bmo, boa],
+        ['td', 'bmo', 'boa'],
+        '2022/12/01'
+    )
+    utils.qxlpyPlotDataFrame(dframe, "line", "dummy", True)
--- quant/utils.py
+++ quant/utils.py
@@ -2,16 +2,89 @@
 Qxlpy Utilities
 """
 from typing import List, Dict
+import pandas as pd
+import plotly.express as px
+
+from quant import py_logger
+from . import global_obj
 
 # pylint: disable=invalid-name
 
 
 def qxlpyCSharpAutogenTest(
-    multiplier: float, num_list: List[int], mix_dict: Dict[str, float])-> Dict[str, float]:
+        num_list: List[int], mix_dict: Dict[str, float],
+        multiplier: float = 3.14, overwrite: bool = True
+    )-> Dict[str, float]:
     """
     Test CSharp autoget script
     """
-    for ea_int in num_list:
-        mix_dict[str(ea_int)] = ea_int * multiplier
-
+    if overwrite:
+        for ea_int in num_list:
+            mix_dict[str(ea_int)] = ea_int * multiplier
+    else:
+        for ea_int in num_list:
+            if not str(ea_int) in mix_dict:
+                mix_dict[str(ea_int)] = ea_int * multiplier
     return mix_dict
+
+def qxlpyPlotDataFrame(
+        dframe_obj: str,
+        plot_type: str = "line",
+        title: str = "Data Points with Plotly",
+        dry_run: bool = False
+    )-> str:
+    """
+    Plot cached Pandas DataFrame obj
+    startdate has to be in string, such as: '2022/12/01'
+    """
+    plot_obj = {
+        "line": px.line,
+        "bar": px.bar,
+        "scatter": px.scatter
+    }
+    if plot_type not in plot_obj:
+        errmsg = f'Plot type "{plot_type}" is not supported. Plot types: {list(plot_obj.keys())}'
+        py_logger.error(errmsg)
+        raise KeyError(errmsg)
+    fig = plot_obj[plot_type](global_obj.get_obj(dframe_obj), title=title)
+    if not dry_run:
+        fig.show()
+    return 'SUCCESS'
+
+def qxlpyCreatePlotDataFrame(
+        cached_arrays: List[str], labels: List[str], startdate: str
+    )-> str:
+    """
+    Take a list of cached List[float] objs and create a DataFrame
+    startdate must be in string format that looks like 2022/12/01
+    """
+    cached_objs = global_obj.list_objs()
+    plot_obj = {}
+    periods = None
+    for pos in range(len(cached_arrays)):  # pylint: disable=consider-using-enumerate
+        handle = cached_arrays[pos]
+        # check obj exists
+        if not handle in cached_objs:
+            errmsg = f'"{handle}" does not exist in cache'
+            py_logger.error(errmsg)
+            raise KeyError(errmsg)
+
+        plot_obj[labels[pos]] = global_obj.get_obj(handle)
+
+        # check obj type
+        if not isinstance(plot_obj[labels[pos]], list):
+            errmsg = f'"{handle}" is not of type "list"'
+            py_logger.error(errmsg)
+            raise TypeError(errmsg)
+        # check obj length
+        if not periods:
+            periods = len(plot_obj[labels[pos]])
+        if len(plot_obj[labels[pos]]) != periods:
+            errmsg = f'{cached_arrays} cannot be jagged: all elements must have the same length'
+            py_logger.error(errmsg)
+
+    df = pd.DataFrame(
+        plot_obj,
+        index = pd.date_range(start=startdate, freq="M", periods=periods)
+    )
+    return global_obj.store_obj(df)
